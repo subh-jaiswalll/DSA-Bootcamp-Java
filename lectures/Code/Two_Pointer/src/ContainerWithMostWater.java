8e6987c7e5619e966c981cad85f4d567438a6ac5
atulcodes-tech <subhkaranjaiswal41@gmail.com>
2026-05-10 12:08:09 +0530

    Container Water Done

--- lectures/Code/Two_Pointer/src/ContainerWithMostWater.java
+++ lectures/Code/Two_Pointer/src/ContainerWithMostWater.java
@@ -24,11 +24,41 @@ public class ContainerWithMostWater {
         return maxWater;
     }
 
+    static int containerWithMostWaterTwoPointer(int[] height){
+        int n = height.length;
+
+        int left = 0;
+        int right= n - 1;
+        int maxWater = 0;
+
+        while(left < right){
+
+            int h = Math.min(height[left], height[right]);
+
+            int w = right - left;
+
+            int water = h * w;
+
+            maxWater = Math.max(maxWater, w);
+
+            if(height[left] < height[right]){
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return maxWater;
+    }
+
     public static void main(String[] args) {
 
         int[] arr = {1,8,6,2,5,4,8,3,7};
 
         int result = containerWithMostWaterBruteForce(arr);
+        System.out.println(result);
+
+        int resultTwoPointer = containerWithMostWaterTwoPointer(arr);
         System.out.println(result);
     }
 }
