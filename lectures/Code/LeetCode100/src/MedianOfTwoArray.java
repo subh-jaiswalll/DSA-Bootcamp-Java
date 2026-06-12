0a88e81358602c5f297dc64092d2ae327be92163
atulcodes-tech <subhkaranjaiswal41@gmail.com>
2026-06-12 13:08:12 +0530

    Done

--- lectures/Code/LeetCode100/src/MedianOfTwoArray.java
+++ lectures/Code/LeetCode100/src/MedianOfTwoArray.java
@@ -1,6 +1,50 @@
 public class MedianOfTwoArray {
 
+
+    public static double medianTwoArray(int[] arr1, int[] arr2){
+
+        int m = arr1.length;
+        int n = arr2.length;
+        int[] merge = new int[m + n];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < m && j < n){
+
+            if(arr1[i] <= arr2[j]){
+                merge[k++] = arr1[i++];
+            }
+            else {
+                merge[k++] = arr2[j++];
+            }
+        }
+
+        while (i < m){
+            merge[k++] = arr1[i++];
+        }
+
+        while (j < n){
+            merge[k++] = arr2[j++];
+        }
+
+        int len = m + n;
+
+        if (len % 2 == 1) {
+            return merge[len / 2];
+        }
+
+        return  (merge[len / 2] + merge[len/ 2 - 1]) / 2.0;
+    }
+
     public static void main(String[] args) {
+
+        int[] arr1 = {1, 3};
+        int[] arr2 = {2 };
+
+        double result = medianTwoArray(arr1, arr2);
+        System.out.println(result);
 
     }
 }
