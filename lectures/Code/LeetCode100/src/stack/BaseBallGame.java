e9d56c28e4b2f4c34b0e7f62449f4f7456bf21cd
atulcodes-tech <subhkaranjaiswal41@gmail.com>
2026-06-30 10:44:33 +0530

    Done

--- lectures/Code/LeetCode100/src/stack/BaseBallGame.java
+++ lectures/Code/LeetCode100/src/stack/BaseBallGame.java
@@ -1,4 +1,44 @@
 package stack;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class BaseBallGame {
+
+    public static int baseballGame(String[] op){
+
+        Stack<Integer> st = new Stack<>();
+
+        for(String ops : op) {
+
+            if (ops.equals("+")) {
+                int top = st.pop();
+                int newTop = top + st.peek();
+                st.push(top);
+                st.push(newTop);
+            } else if (ops.equals("C")) {
+                st.pop();
+            } else if (ops.equals("D")) {
+                st.push(2 * st.peek());
+            } else {
+                st.push(Integer.parseInt(ops));
+            }
+
+        }
+            int ans = 0;
+
+            while (!st.isEmpty()){
+                ans+=st.pop();
+            }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        String[] str = {"5","-2","4","C","D","9","+","+"};
+
+        int result = baseballGame(str);
+        System.out.println(result);
+    }
 }
